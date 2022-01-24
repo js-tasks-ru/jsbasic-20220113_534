d0269ef7ff60f78373869006eb98cf9ea3d5f041
KATERINA KRASNITSKAYA <zzzchapazzz@gmail.com>
2022-01-24 20:38:20 +0100

    fix4add5

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,20 +1,4 @@
 function checkSpam(str) {
-    let flag = false
-    let flag2 = false
     str = str.toUpperCase();
-    for (let i = 0; i <= str.length - 3; i++) {
-        if (str.substring(i, i + 3) === "XXX") {
-            flag = true
-        }
-    }
-    for (let i = 0; i <= str.length - 5; i++) {
-        if (str.substring(i, i + 5) === "1XBET") {
-            flag2 = true
-        }
-    }
-    if (flag || flag2) {
-        return true
-    } else {
-        return false
-    }
+    return str.includes("XXX") || str.includes("1XBET")
 }

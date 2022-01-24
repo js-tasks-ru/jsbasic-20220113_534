02ebb8087bec065f16cd12a5d4492d2fbd31e623
KATERINA KRASNITSKAYA <zzzchapazzz@gmail.com>
2022-01-24 09:44:24 +0100

    Module1Task4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,20 @@
 function checkSpam(str) {
-  // ваш код...
+    let flag = false
+    let flag2 = false
+    str = str.toUpperCase();
+    for (let i = 0; i <= str.length - 3; i++) {
+        if (str.substring(i, i + 3) === "XXX") {
+            flag = true
+        }
+    }
+    for (let i = 0; i <= str.length - 5; i++) {
+        if (str.substring(i, i + 5) === "1XBET") {
+            flag2 = true
+        }
+    }
+    if (flag || flag2) {
+        return true
+    } else {
+        return false
+    }
 }

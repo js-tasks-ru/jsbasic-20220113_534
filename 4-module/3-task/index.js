0e4d21635e18862ec34562b9a25db6cad9889fda
KATERINA KRASNITSKAYA <zzzchapazzz@gmail.com>
2022-02-13 21:21:39 +0100

    Module4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+    let trs = table.querySelectorAll('tr')
+    for (let [i, row] of trs.entries()) {
+        let tds = row.querySelectorAll('td')
+        let a = tds[tds.length - 1].getAttribute('data-available')
+        if (a == "true") {
+            row.classList.add('available')
+        }
+        if (a == "false") {
+            row.classList.add('unavailable')
+        }
+        if (!a && i != 0) {
+            row.hidden = 'true'
+        }
+        let b = tds[tds.length - 2].innerHTML
+        if (b == "m") {
+            row.classList.add('male')
+        }
+        if (b == "f") {
+            row.classList.add('female')
+        }
+        let c = tds[tds.length - 3].innerHTML
+        if (c <= 18) {
+            row.style = "text-decoration: line-through"
+        }
+
+    }
+
 }
